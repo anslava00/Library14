39127a473d56c74ee564a810ac0036a87f7b9fff
anslava00 <anslava00@mail.ru>
2022-09-13 22:16:25 +1000

    Fix book title, add other request

---
+++ src/Controller/RequestsController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Entity\Book;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RequestsController extends AbstractController
+{
+    /**
+     * @Route("/requests", name="requests")
+     */
+    public function index(Request $request): Response
+    {
+        $books = [];
+        if ($request->isMethod('POST')) {
+            if (isset($_POST['back']))
+                return $this->redirectToRoute('mainpage');
+            if (isset($_POST['ORM']))
+            {
+                $books = $this->getDoctrine()->getRepository(Book::class)->findOrherRequestORM();
+            }
+            elseif (isset($_POST['SQL']))
+            {
+                $books = $this->getDoctrine()->getRepository(Book::class)->findOrherRequestSQL();
+            }
+            elseif (isset($_POST['ALL']))
+            {
+                $books = $this->getDoctrine()->getRepository(Book::class)->findAll();
+            }
+        }
+        return $this->render('requests/index.html.twig', [
+            'books' => $books,
+        ]);
+    }
+}
